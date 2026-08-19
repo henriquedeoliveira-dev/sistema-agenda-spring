3f9f1eb6a25df89e3548fc095fd8ce663338cbec
henriqueoliveira-dev <henriquepantaleaodeoliveira@gmail.com>
2026-08-19 18:14:41 -0300

    refactor: refatora a classe ErroResposta

--- src/main/java/com/henriquedeoliveira_dev/sistema_agenda_spring/exception/ErroResposta.java
+++ src/main/java/com/henriquedeoliveira_dev/sistema_agenda_spring/exception/ErroResposta.java
@@ -1,33 +1,20 @@
 package com.henriquedeoliveira_dev.sistema_agenda_spring.exception;
 
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
+@Getter
 public class ErroResposta {
-    private LocalDateTime timestamp;
-    private int status;
-    private String erro;
-    private String mensagem;
+    private final LocalDateTime timestamp;
+    private final int status;
+    private final String erro;
+    private final String mensagem;
 
     public ErroResposta(int status, String erro, String mensagem) {
         this.timestamp = LocalDateTime.now();
         this.status = status;
         this.erro = erro;
         this.mensagem = mensagem;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public String getErro() {
-        return erro;
-    }
-
-    public String getMensagem() {
-        return mensagem;
     }
 }
